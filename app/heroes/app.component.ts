302e599e6ae482508ac8e3deca50e472e93cb707
kumudug <kumudug1@gmail.com>
2017-08-06 21:20:34 +1000

    rouing fix intermediate

--- app/heroes/app.component.ts
+++ app/heroes/app.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-    template: `
+  template: `
       <h1>{{title}}</h1>
       <nav>
         <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
         <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
       </nav>
       <router-outlet></router-outlet>
+      <div ng-view class="view-frame"></div>
+      <div class="view-container">
+        
+      </div>
     `,
-    styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- app/heroes/app.module.ts
+++ app/heroes/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { RouterModule } from '@angular/router';
+import { RouterModule, UrlHandlingStrategy } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -10,6 +10,17 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 import { AppRoutingModule } from './app-routing.module';
+
+class Ng1Ng2UrlHandlingStrategy implements UrlHandlingStrategy {
+  //shouldProcessUrl(url) { return url.toString().startsWith("/settings"); }
+  shouldProcessUrl(url) { 
+    var temp = url.toString().includes("/heroes"); 
+    console.log(url.toString() + ' - ' + temp);
+    return temp;
+  }
+  extract(url) { return url; }
+  merge(url, whole) { return url; }
+}
 
 @NgModule({
   imports: [
@@ -24,7 +35,10 @@ import { AppRoutingModule } from './app-routing.module';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [HeroService],
+  providers: [
+    HeroService,
+    { provide: UrlHandlingStrategy, useClass: Ng1Ng2UrlHandlingStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
